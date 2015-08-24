e18401dba4041515aa9fe7ee385bdd98cc889b5f
iEason <tianzhenkuan@qq.com>
2015-08-24 18:01:25 +0800

    nsobject category

---
+++ IECore/Fundation+IE/NSObject+IE.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+IE.h
+//  IECore
+//
+//  Created by Eason on 15/8/24.
+//  Copyright (c) 2015年 iEason. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (IE)
+
+@end
---
+++ IECore/Fundation+IE/NSObject+IE.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+IE.m
+//  IECore
+//
+//  Created by Eason on 15/8/24.
+//  Copyright (c) 2015年 iEason. All rights reserved.
+//
+
+#import "NSObject+IE.h"
+
+@implementation NSObject (IE)
+
+@end
