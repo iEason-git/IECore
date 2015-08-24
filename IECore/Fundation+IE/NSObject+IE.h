949b602ed04a6b847412f60010f6b2224723924b
iEason <tianzhenkuan@qq.com>
2015-08-24 18:04:40 +0800

    commit

--- IECore/Fundation+IE/NSObject+IE.h
+++ IECore/Fundation+IE/NSObject+IE.h
@@ -10,4 +10,37 @@
 
 @interface NSObject (IE)
 
+#pragma mark -
+#pragma mark # object
+@property (nonatomic, strong) id    tempObject;
+
+//bind object
+//setbind value
+- (id)bindObject:(const char *)identifier;
+- (void)setBindObject:(const char *)identifier object:(id)obj;
+
+// send object
+// handle block with default identifier is @"sendObject".
+- (void)sendObject:(id)anObject;
+- (void)receiveObject:(void(^)(id object))aBlock;
+
+//tag can't be nil
+- (void)sendObject:(id)anObject withIdentifier:(NSString *)identifier;
+- (void)receiveObject:(void(^)(id object))aBlock withIdentifier:(NSString *)identifier;
+
+#pragma mark -
+#pragma mark # block
+- (id)blockForDefaultEvent;
+// handle block with default identifier is @"EventBlock".
+- (void)handlerDefaultEventWithBlock:(id)aBlock;
+
+// 设置一个block作为回调
+- (void)handlerEventWithBlock:(id)aBlock withIdentifier:(NSString *)identifier;
+- (id)blockForEventWithIdentifier:(NSString *)identifier;
+
+#pragma mark -
+#pragma mark # copy
+// 基于NSKeyArchive.如果 self导入XYAutoCoding.h,可用与自定义对象
+- (id)deepCopy;
+
 @end
--- IECore/Fundation+IE/NSObject+IE.m
+++ IECore/Fundation+IE/NSObject+IE.m
@@ -7,7 +7,119 @@
 //
 
 #import "NSObject+IE.h"
+#import <objc/runtime.h>
+
+#define NSObject_key_tempObject	"NSObject.tempObject"
+#define NSObject_key_objectDic	"NSObject.objectDic"
+#define NSObject_key_EventBlockDic	"NSObject.eventBlockDic"
 
 @implementation NSObject (IE)
+
+#pragma mark- object
+- (id)tempObject
+{
+    return objc_getAssociatedObject(self, NSObject_key_tempObject);
+}
+
+- (void)setTempObject:(id)tempObject
+{
+    [self willChangeValueForKey:@"tempObject"];
+    
+    objc_setAssociatedObject(self, NSObject_key_tempObject, tempObject, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    
+    [self didChangeValueForKey:@"tempObject"];
+}
+
+- (id)bindObject:(const char *)identifier;
+{
+    NSAssert(identifier != nil, @"identifier can't be nil.");
+    
+    return objc_getAssociatedObject(self, identifier);
+}
+
+- (void)setBindObject:(const char *)identifier object:(id)obj
+{
+    NSAssert(identifier != nil, @"identifier can't be nil.");
+    
+    objc_setAssociatedObject(self, identifier, obj, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (void)sendObject:(id)anObject
+{
+    [self sendObject:anObject withIdentifier:@"sendObject"];
+}
+
+- (void)receiveObject:(void(^)(id object))aBlock
+{
+    [self receiveObject:aBlock withIdentifier:@"sendObject"];
+}
+
+- (void)sendObject:(id)anObject withIdentifier:(NSString *)identifier
+{
+    NSAssert(identifier != nil, @"identifier can't be nil.");
+    
+    NSDictionary *dic = objc_getAssociatedObject(self, NSObject_key_objectDic);
+    
+    if(dic == nil) return;
+    
+    void(^aBlock)(id anObject) =  [dic objectForKey:identifier];
+    aBlock(anObject);
+}
+
+- (void)receiveObject:(void(^)(id object))aBlock withIdentifier:(NSString *)identifier
+{
+    NSAssert(identifier != nil, @"identifier can't be nil.");
+    
+    NSMutableDictionary *dic = objc_getAssociatedObject(self, NSObject_key_objectDic);
+    if(dic == nil)
+    {
+        dic = [NSMutableDictionary dictionaryWithCapacity:4];
+        objc_setAssociatedObject(self, NSObject_key_objectDic, dic, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    }
+    
+    [dic setObject:[aBlock copy] forKey:identifier];
+}
+
+#pragma mark - block
+- (id)blockForDefaultEvent
+{
+    return [self blockForEventWithIdentifier:@"EventBlock"];
+}
+
+- (void)handlerDefaultEventWithBlock:(id)block
+{
+    [self handlerEventWithBlock:block withIdentifier:@"EventBlock"];
+}
+
+- (void)handlerEventWithBlock:(id)aBlock withIdentifier:(NSString *)identifier
+{
+    NSAssert(identifier != nil, @"identifier can't be nil.");
+    
+    NSMutableDictionary *dic = objc_getAssociatedObject(self, NSObject_key_EventBlockDic);
+    if(dic == nil)
+    {
+        dic = [[NSMutableDictionary alloc] init];
+        objc_setAssociatedObject(self, NSObject_key_EventBlockDic, dic, OBJC_ASSOCIATION_RETAIN);
+    }
+    
+    [dic setObject:[aBlock copy] forKey:identifier];
+}
+
+- (id)blockForEventWithIdentifier:(NSString *)identifier
+{
+    NSAssert(identifier != nil, @"identifier can't be nil.");
+    
+    NSDictionary *dic = objc_getAssociatedObject(self, NSObject_key_EventBlockDic);
+    
+    if(dic == nil) return nil;
+    
+    return [dic objectForKey:identifier];
+}
+
+#pragma mark- copy
+- (id)deepCopy
+{
+    return [NSKeyedUnarchiver unarchiveObjectWithData:[NSKeyedArchiver archivedDataWithRootObject:self]];
+}
 
 @end
