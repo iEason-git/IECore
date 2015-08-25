aa589224fb1414e33acd7324817e2c90f461204a
iEason <tianzhenkuan@qq.com>
2015-08-25 13:00:09 +0800

    add nsstring

---
+++ IECore/Foundation+IE/NSString+IE.h
@@ -0,0 +1,13 @@
+//
+//  NSString+IE.h
+//  IECore
+//
+//  Created by Eason on 15/8/25.
+//  Copyright (c) 2015年 iEason. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (IE)
+
+@end
---
+++ IECore/Foundation+IE/NSString+IE.m
@@ -0,0 +1,13 @@
+//
+//  NSString+IE.m
+//  IECore
+//
+//  Created by Eason on 15/8/25.
+//  Copyright (c) 2015年 iEason. All rights reserved.
+//
+
+#import "NSString+IE.h"
+
+@implementation NSString (IE)
+
+@end
